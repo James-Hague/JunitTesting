f01094a8abbe3f60a32985ce64c365a7f03c93b3
James-Hague <33607309+James-Hague@users.noreply.github.com>
2018-01-09 16:33:12 +0000

    update

--- src/test/java/ChromeDriverTestTest.java
+++ src/test/java/ChromeDriverTestTest.java
@@ -1,4 +1,5 @@
 
+import jdk.nashorn.internal.ir.Block;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -18,17 +19,38 @@ public class ChromeDriverTestTest {
             System.setProperty("webdriver.chrome.driver", "C:/Users/James/Desktop/QA_Software/chromedriver_win32/chromedriver.exe");
 
             WebDriver driver = new ChromeDriver();
-            driver.get("https://www.qa.com/");
+            driver.get("http://thedemosite.co.uk/");
             String targetURL = driver.getCurrentUrl();
 
             Thread.sleep(3000);  // Let the user actually see something!
-            WebElement searchBox = driver.findElement(By.name("q"));
+            driver.get("http://thedemosite.co.uk/addauser.php");
+            Thread.sleep(5000);  // Let the user actually see something!
+            //WebElement searchBox = driver.findElement(By.name("q"));
             //searchBox.sendKeys("ChromeDriver");
             //searchBox.submit();
-            Thread.sleep(3000);  // Let the user actually see something!
+            // USERNAME
+            WebElement userName = driver.findElement(By.name("username"));
+            userName.sendKeys("BobRoss");
+            WebElement userPassword = driver.findElement(By.name("password"));
+            userPassword.sendKeys("BobPassword");
+            WebElement addUser = driver.findElement(By.name("FormsButton2"));
+            addUser.click();
+            Thread.sleep(5000);  // Let the user actually see something!
+            // LOGIN
+            driver.get("http://thedemosite.co.uk/login.php");
+            WebElement loginUser = driver.findElement(By.name("username"));
+            loginUser.sendKeys("BobRoss");
+            WebElement enterPassword = driver.findElement(By.name("password"));
+            enterPassword.sendKeys("BobPassword");
+            WebElement logIn = driver.findElement(By.name("FormsButton2"));
+            logIn.click();
+            Thread.sleep(5000);  // Let the user actually see something!
+            WebElement Succes = driver.findElement(By.cssSelector("body > table > tbody > tr > td.auto-style1 > big > blockquote > blockquote > font > center > b"));
 
-            assertEquals(targetURL, "https://www.qa.com/");
-            System.out.println("Twan sucks");
+            assertEquals(Succes.getText(), "**Successful Login**");
+
+            //assertEquals(targetURL, "https://www.qa.com/");
+            System.out.println("assert win");
             driver.quit();
         }
 
