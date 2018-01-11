ca7360c8b4487f40b32ad5ba6edd15534b000698
James-Hague <33607309+James-Hague@users.noreply.github.com>
2018-01-11 15:51:10 +0000

    create cookie file FORMATTED

--- src/test/java/CookiesTest.java
+++ src/test/java/CookiesTest.java
@@ -1,42 +1,18 @@
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class CookiesTest {
 
 
     //DRIVER SET UP
-    //String password = CookiesTest.scan();
     WebDriver driver;
-    //Actions builder = new Actions(driver);
-
-    public static String scan() {
-
-        //PASSWORD SCANNER INPUT
-        System.out.print("Enter Password: ");
-        Scanner EnterPass = new Scanner(System.in);
-        String passInput;
-        passInput = EnterPass.nextLine();
-        return passInput;
-
-    }
-
-
-
-    @Before
-    public void setUp() throws Exception {
-
-    }
 
     @Test
     public void Test() throws InterruptedException, IOException {
@@ -44,6 +20,7 @@ public class CookiesTest {
 
         driver = new ChromeDriver();
         System.setProperty("webdriver.chrome.driver", "C:/Users/James/Desktop/QA_Software/chromedriver_win32/chromedriver.exe");
+
         //LOAD SITE
         driver.get("https://www.amazon.co.uk/ref=nav_logo/258-8646541-4617941");
         Thread.sleep(1000);
@@ -54,15 +31,12 @@ public class CookiesTest {
         f.createNewFile();
         BufferedWriter bos = new BufferedWriter(new FileWriter(f));
 
-        for(Cookie ck : driver.manage().getCookies()) {
-            bos.write((ck.getName()+";"+ck.getValue()+";"+ck.getDomain()
-                    +";"+ck.getPath()+";"+ck.getExpiry()+";"+ck.isSecure()));
+        for (Cookie ck : driver.manage().getCookies()) {
+            bos.write((ck.getName() + ";" + ck.getValue() + ";" + ck.getDomain()
+                    + ";" + ck.getPath() + ";" + ck.getExpiry() + ";" + ck.isSecure()));
             bos.newLine();
         }
         // READ COOKIE
-
-
-
 
 
         // LOGIN
@@ -73,16 +47,10 @@ public class CookiesTest {
         driver.findElement(By.cssSelector("#signInSubmit")).click();
         Thread.sleep(3000);
 
-
-
-
+        // QUIT
         driver.quit();
 
 
-
     }
 
-    @After
-    public void tearDown() throws Exception {
-    }
 }
